3357b133b661d9124ef3ea6019b98f9bae798016
limescode <katechizis@gmail.com>
2022-11-13 17:03:34 +0100

    initial commit

---
+++ src/main/java/pl/limescode/gkboot/GkBootApplication.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/limescode/gkboot/GkBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
